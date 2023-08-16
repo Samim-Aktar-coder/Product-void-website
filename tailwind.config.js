6380a36bdd158e1b05dcc63caa72da212a1229e4
Samim Aktar <samimaktar94320@gmail.com>
2023-08-16 17:21:24 +0530

    All file added

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*','./*/*'],
+  theme: {
+    screens: {
+      'ss': '470px',
+      'sm': '650px',
+      'md': '770px',
+      'lg': '1050px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    }
+  },
+  plugins: [],
+}
+
